268babcd13152ec47245b7df4139d2840bc8987b
Hugz12 <hugo.laloy@gmail.com>
2025-06-03 11:33:09 +0200

    feat: Création de fichiers centralisés pour la gestion des erreurs et des succès

---
+++ src/types/successResponses.ts
@@ -0,0 +1,12 @@
+export const SuccessResponses = {
+    USER_REGISTERED: {
+        statusCode: 201,
+        message: "User registered successfully",
+        internalCode: "USER_REGISTERED"
+    },
+    USER_LOGGED_IN: {
+        statusCode: 200,
+        message: "User logged in successfully",
+        internalCode: "USER_LOGGED_IN"
+    },
+}
