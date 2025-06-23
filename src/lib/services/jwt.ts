b0b31c66e1ba9c6b5b1348712cf88efee95cd851
Hugz12 <hugo.laloy@gmail.com>
2025-06-23 12:39:32 +0200

    doc: Add detailed JWT functionality explanation in comments

--- src/lib/services/jwt.ts
+++ src/lib/services/jwt.ts
@@ -27,3 +27,28 @@ export function verifyToken<T = unknown>(token: string): T | null {
 		return null;
 	}
 }
+
+/** Fonctionnement du JWT
+ *  
+ *  Le JWT (JSON Web Token) est un jeton sécurisé utilisé pour l’authentification, composé de trois parties :
+ *   - **Header** : Indique le type de jeton et l’algorithme de signature.
+ *   - **Payload** : Contient les données (claims) comme l’ID utilisateur
+ *   - **Signature** : Assure l’intégrité du jeton en le signant avec une clé secrète.
+ * 
+ *   - Header = base64UrlEncode({
+ *       "alg": "HS256",
+ *       "typ": "JWT"
+ *   })
+ * 
+ *    - Payload = base64UrlEncode({
+ *       "idUtilisateur": 123,
+ *       "login": "user123",
+ *       "iat": 1700000000, // Date de création du token
+ *       "exp": 1700003600 // Date d'expiration du token
+ *    })
+ * 
+ *    - Signature = HMACSHA256(Header + "." + Payload, secret)
+ * 
+ *    Le jeton final est une concaténation des trois parties, séparées par des points :
+ *    `Header.Payload.Signature`
+ */
